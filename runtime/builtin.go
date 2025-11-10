3e4f0edd1307276e35365d9441ea36ffd9f8d1c9
한진희 <0200134hjh@gmail.com>
2025-11-10 11:56:14 +0900

    Update builtin.go

--- runtime/builtin.go
+++ runtime/builtin.go
@@ -2,19 +2,36 @@ package runtime
 
 import "fmt"
 
-// 함수 호출 규약
+type BuiltinFunc func(...*HeapObject) *HeapObject
+
+var builtins = map[string]BuiltinFunc{
+	"add": Add,
+	"sub": Sub,
+}
+
+func GetBuiltin(name string) BuiltinFunc {
+	if f, ok := builtins[name]; ok {
+		return f
+	}
+	return nil
+}
+
 func Call(fn func(...*HeapObject) *HeapObject, args ...*HeapObject) *HeapObject {
 	return fn(args...)
 }
 
-// 내장 Add
 func Add(args ...*HeapObject) *HeapObject {
 	a := args[0].Data.(int)
 	b := args[1].Data.(int)
 	return Alloc("int", a+b)
 }
 
-// Print 함수 (Rust의 println! 매핑)
+func Sub(args ...*HeapObject) *HeapObject {
+	a := args[0].Data.(int)
+	b := args[1].Data.(int)
+	return Alloc("int", a-b)
+}
+
 func Print(o *HeapObject) {
 	fmt.Printf("[PRINT] %v (%s)\n", o.Data, o.Type)
 }
